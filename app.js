97994e14507c898a3a02c18a9f7714e1fd9bb744
Jonathan Smiley <92873891+jonnysmiley@users.noreply.github.com>
2021-12-29 10:31:07 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark")
+    document.querySelector(".eman").classList.toggle("dark")
+});
+
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active')
+  mobile_menu.classList.toggle('is-active')
+});
